cc4e2cc205b9a6072bc705726d797178edacc527
wengyao <wengyao@163.com>
2016-04-04 20:41:54 +0800

    增加可编辑

--- MyPage/lib/myLibs/edit.js
+++ MyPage/lib/myLibs/edit.js
@@ -1,22 +1,42 @@
-$(document).ready(function(){
-	$(document).on("click",".addRowBtn",function(){
+$(document).ready(function() {
+	$(document).on("click", ".addRowBtn", function() {
 		addRow();
 	});
-	$(document).on("click",".addColBtn",function(){
+	$(document).on("click", ".addColBtn", function() {
 		addCol();
 	});
+
+	$(document).on("click", ".attr_wrap td", function() {
+		var $td = $(this);
+		if ($td.hasClass("focus")) {
+			$td.find("input").focus();
+			return false;
+		};
+		var $focus = $(".attr_wrap td.focus");
+		if ($focus.length > 0) {
+			$focus.removeClass("focus")
+			$focus.html($focus.find(".tableEditInput").val());
+		}
+		var $input = $(document.createElement("input"));
+		$input.addClass("tableEditInput");
+		$input.val($td.html());
+		$td.html($input).addClass("focus");
+		$input.focus();
+		console.log(123123);
+	});
+
 });
 
-function addRow(){
+function addRow() {
 	var $newTR = $(".product_wrap tr").last().clone();
 	//根据业务逻辑对 $newTR 进行修改属性。
 	$(".product_wrap").append($newTR);
 }
 
-function addCol(){
-	$(".product_wrap tr").each(function(i){
-		var $newTD = $(document.createElement("td")  ); //根据业务逻辑进行修改
+function addCol() {
+	$(".product_wrap tr").each(function(i) {
+		var $newTD = $(document.createElement("td")); //根据业务逻辑进行修改
 		$newTD.html(i);
-		$(this).append( $newTD );
+		$(this).append($newTD);
 	});
 }
